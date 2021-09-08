3566e133e0938b1db6ad79e3c34f4ca2a091fe0c
Kiều Vũ Minh Đức <kvmduc3@gmail.com>
2021-09-08 12:52:31 +0700

    utils and k-means

---
+++ source/model/utils.py
@@ -0,0 +1,32 @@
+import numpy as np
+import random
+import pandas as pd
+
+def generate_playlist(pd_track, pd_playlist, num_playlist_to_test = 100):
+    playlist_choices = random.choices(np.arange(pd_playlist.shape[0]), k = num_playlist_to_test)
+
+    playlist_ids = []
+
+    for playlist_index in playlist_choices:
+        # a = pd_track[pd_playlist.loc[playlist_index, 'playlist_id'] == pd_track['playlist_id']]
+        playlist_ids.append(pd_playlist.loc[playlist_index, 'playlist_id'])
+    
+    track_ids = {}
+    for list_id in playlist_ids:
+        track_ids[list_id] = list(pd_track[pd_track['playlist_id'] == list_id]['track_id'])
+    return track_ids
+
+def generate_test_playlist(track_ids, missing_rate = 0.2):
+    track_id_for_test = {}
+    for key in track_ids:
+        all_tracks = track_ids[key]
+        n = len(all_tracks)
+        nums_songs_to_test = int(n * missing_rate)
+        track_id_for_test[key] = all_tracks[:nums_songs_to_test]
+    return track_id_for_test
+
+# pd_playlist = pd.read_csv('../data/20210824_212829_playlists.tsv', sep='\t')
+# pd_track = pd.read_csv('../data/20210824_212829_tracks.tsv', sep='\t')
+
+# playlist_test = generate_playlist(pd_track, pd_playlist)
+# test = generate_test_playlist(playlist_test)
