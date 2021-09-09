8334275ebf74ec74055a1db7c38213be82f0a282
Kiều Vũ Minh Đức <kvmduc3@gmail.com>
2021-09-09 21:19:41 +0700

    update model

--- source/model/utils.py
+++ source/model/utils.py
@@ -8,8 +8,9 @@ def generate_playlist(pd_track, pd_playlist, num_playlist_to_test = 100):
     playlist_ids = []
 
     for playlist_index in playlist_choices:
-        # a = pd_track[pd_playlist.loc[playlist_index, 'playlist_id'] == pd_track['playlist_id']]
-        playlist_ids.append(pd_playlist.loc[playlist_index, 'playlist_id'])
+        a = pd_track[pd_playlist.loc[playlist_index, 'playlist_id'] == pd_track['playlist_id']].shape[0]
+        if(a >= 30):
+            playlist_ids.append(pd_playlist.loc[playlist_index, 'playlist_id'])
     
     track_ids = {}
     for list_id in playlist_ids:
@@ -21,12 +22,18 @@ def generate_test_playlist(track_ids, missing_rate = 0.2):
     for key in track_ids:
         all_tracks = track_ids[key]
         n = len(all_tracks)
-        nums_songs_to_test = int(n * missing_rate)
+        nums_songs_to_test = int(n * (1 - missing_rate))
         track_id_for_test[key] = all_tracks[:nums_songs_to_test]
     return track_id_for_test
 
-# pd_playlist = pd.read_csv('../data/20210824_212829_playlists.tsv', sep='\t')
-# pd_track = pd.read_csv('../data/20210824_212829_tracks.tsv', sep='\t')
+def r_precision(prediction, label):
+	prediction = list(set(prediction))
+	label = list(set(label))
+	score = len(list(set(prediction) & set(label))) / len(label)
+	return score
 
-# playlist_test = generate_playlist(pd_track, pd_playlist)
+pd_playlist = pd.read_csv('../data/20210824_212829_playlists.tsv', sep='\t')
+pd_track = pd.read_csv('../data/20210824_212829_tracks.tsv', sep='\t')
+
+playlist_test = generate_playlist(pd_track, pd_playlist)
 # test = generate_test_playlist(playlist_test)
